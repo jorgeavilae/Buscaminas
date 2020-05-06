660c188496d24bbf501dc6646fa5344c210498aa
jorgeavilae <jorgeavilae@gmail.com>
2020-05-06 23:10:07 +0200

    Main Fragment UI connect with ViewModel and updates when pressing arrows

--- app/src/main/java/com/jorgeav/buscaminas/ui/main/MainFragment.kt
+++ app/src/main/java/com/jorgeav/buscaminas/ui/main/MainFragment.kt
@@ -1,13 +1,15 @@
 package com.jorgeav.buscaminas.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.jorgeav.buscaminas.R
+import com.jorgeav.buscaminas.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -15,17 +17,63 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
+    private lateinit var binding : MainFragmentBinding
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
+        binding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        viewModel.rows.observe(viewLifecycleOwner, Observer { rows ->
+            rows?.let {
+                enabledRowsButtons(rows)
+            }
+        })
+        viewModel.columns.observe(viewLifecycleOwner, Observer { columns ->
+            columns?.let {
+                enabledColumnsButtons(columns)
+            }
+        })
+
+        return binding.root
     }
 
+    private fun enabledRowsButtons(rows: Int) {
+        when {
+            rows <= MIN_ROWS -> {
+                binding.lessRowsButton.isClickable = false
+                binding.moreRowsButton.isClickable = true
+            }
+            rows >= MAX_ROWS -> {
+                binding.lessRowsButton.isClickable = true
+                binding.moreRowsButton.isClickable = false
+            }
+            else -> {
+                binding.lessRowsButton.isClickable = true
+                binding.moreRowsButton.isClickable = true
+            }
+        }
+    }
+
+    private fun enabledColumnsButtons(columns: Int) {
+        when {
+            columns <= MIN_COLUMNS -> {
+                binding.lessColumnsButton.isEnabled = false
+                binding.moreColumnsButton.isEnabled = true
+            }
+            columns >= MAX_COLUMNS -> {
+                binding.lessColumnsButton.isEnabled = true
+                binding.moreColumnsButton.isEnabled = false
+            }
+            else -> {
+                binding.lessColumnsButton.isEnabled = true
+                binding.moreColumnsButton.isEnabled = true
+            }
+        }
+
+    }
 }
--- app/src/main/java/com/jorgeav/buscaminas/ui/main/MainViewModel.kt
+++ app/src/main/java/com/jorgeav/buscaminas/ui/main/MainViewModel.kt
@@ -1,7 +1,45 @@
 package com.jorgeav.buscaminas.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+const val MIN_ROWS = 4
+const val MAX_ROWS = 20
+const val MIN_COLUMNS = 4
+const val MAX_COLUMNS = 20
+
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _rows = MutableLiveData<Int>()
+    val rows : LiveData<Int>
+        get() = _rows
+
+    private val _columns = MutableLiveData<Int>()
+    val columns : LiveData<Int>
+        get() = _columns
+
+    init {
+        _rows.value = MIN_ROWS
+        _columns.value = MIN_COLUMNS
+    }
+
+    fun onLessRowsClicked() {
+        val rows : Int = _rows.value ?: MIN_ROWS + 1
+        _rows.value = if (rows > MIN_ROWS) rows.minus(1) else rows
+    }
+
+    fun onMoreRowsClicked() {
+        val rows : Int = _rows.value ?: MIN_ROWS - 1
+        _rows.value = if (rows < MAX_ROWS) rows.plus(1) else rows
+    }
+
+    fun onLessColumnsClicked() {
+        val columns : Int = _columns.value ?: MIN_COLUMNS + 1
+        _columns.value = if (columns > MIN_COLUMNS) columns.minus(1) else columns
+    }
+
+    fun onMoreColumnsClicked() {
+        val columns : Int = _columns.value ?: MIN_COLUMNS - 1
+        _columns.value = if (columns < MAX_COLUMNS) columns.plus(1) else columns
+    }
 }
