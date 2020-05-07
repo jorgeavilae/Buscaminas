1b6a8d49d73c8d6757fa3070e774e8ccee7f6c05
jorgeavilae <jorgeavilae@gmail.com>
2020-05-07 18:21:41 +0200

    Domain: added Cell data class

---
+++ core/src/main/java/com/jorgeav/buscaminas/domain/Cell.kt
@@ -0,0 +1,13 @@
+package com.jorgeav.buscaminas.domain
+
+/**
+ * Created by Jorge Avila on 07/05/2020.
+ */
+data class Cell (val x : Int, val y : Int,
+                 var isShowing : Boolean = false,
+                 var isMarked : Boolean = false,
+                 var isBomb : Boolean = false,
+                 var numberOfBombsInBounds : Int = 0
+) {
+    val id : String = "$x-$y"
+}
