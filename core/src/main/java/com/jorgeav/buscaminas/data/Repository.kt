2ab26283eda6f91b391bcc7bb47eb18c098017ed
jorgeavilae <jorgeavilae@gmail.com>
2020-05-07 20:19:28 +0200

    UseCases should use Map to handle board of Cells. DataSource should use List, so Repository must perform a cast.

--- core/src/main/java/com/jorgeav/buscaminas/data/Repository.kt
+++ core/src/main/java/com/jorgeav/buscaminas/data/Repository.kt
@@ -7,9 +7,9 @@ import com.jorgeav.buscaminas.domain.Cell
  */
 class Repository(private val dataSource: IPersistentDataSource) {
 
-    suspend fun addAll(cells : List<Cell>) = dataSource.addAll(cells)
+    suspend fun addAll(cells : Map<Pair<Int, Int>, Cell>) = dataSource.addAll(cells.values.toList())
 
-    suspend fun readAll() : List<Cell> = dataSource.readAll()
+    suspend fun readAll() : List<Cell> = dataSource.readAll() //TODO transform to Map???
 
     suspend fun showCell(cell : Cell) = dataSource.showCell(cell.x, cell.y)
 
--- core/src/main/java/com/jorgeav/buscaminas/usecases/GenerateBoardUseCase.kt
+++ core/src/main/java/com/jorgeav/buscaminas/usecases/GenerateBoardUseCase.kt
@@ -6,6 +6,6 @@ import com.jorgeav.buscaminas.domain.Cell
  * Created by Jorge Avila on 07/05/2020.
  */
 class GenerateBoardUseCase() {
-    suspend operator fun invoke(rows : Int, columns : Int, bombs : Int) : List<Cell> =
+    suspend operator fun invoke(rows : Int, columns : Int, bombs : Int) : Map<Pair<Int, Int>, Cell> =
         Cell.generateBoard(rows, columns, bombs)
 }
