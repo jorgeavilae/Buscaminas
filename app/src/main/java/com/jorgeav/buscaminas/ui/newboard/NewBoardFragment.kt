03e576ca4bd17da747173aef6a1d88e557f56cb3
jorgeavilae <jorgeavilae@gmail.com>
2020-05-20 18:58:48 +0200

    NewBoardFragment more/less bombs buttons now work when holding clicks.

--- app/src/main/java/com/jorgeav/buscaminas/ui/newboard/NewBoardFragment.kt
+++ app/src/main/java/com/jorgeav/buscaminas/ui/newboard/NewBoardFragment.kt
@@ -1,22 +1,33 @@
 package com.jorgeav.buscaminas.ui.newboard
 
+import android.annotation.SuppressLint
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.jorgeav.buscaminas.MainActivity
 import com.jorgeav.buscaminas.R
 import com.jorgeav.buscaminas.databinding.NewBoardFragmentBinding
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class NewBoardFragment : Fragment() {
+@SuppressLint("ClickableViewAccessibility")
+class NewBoardFragment : Fragment(), View.OnTouchListener {
 
     private lateinit var binding : NewBoardFragmentBinding
     private lateinit var viewModel: NewBoardViewModel
+
+    private var moreBombsButtonClickedJob : Job? = null
+    private var lessBombsButtonClickedJob : Job? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
@@ -50,6 +61,9 @@ class NewBoardFragment : Fragment() {
             }
         })
 
+        binding.moreBombsButton.setOnTouchListener(this)
+        binding.lessBombsButton.setOnTouchListener(this)
+
         return binding.root
     }
 
@@ -75,10 +89,12 @@ class NewBoardFragment : Fragment() {
             bombs <= viewModel.minBombs -> {
                 binding.lessBombsButton.isEnabled = false
                 binding.moreBombsButton.isEnabled = true
+                lessBombsButtonClickedJob?.cancel()
             }
             bombs >= viewModel.maxBombs -> {
                 binding.lessBombsButton.isEnabled = true
                 binding.moreBombsButton.isEnabled = false
+                moreBombsButtonClickedJob?.cancel()
             }
             else -> {
                 binding.lessBombsButton.isEnabled = true
@@ -86,5 +102,68 @@ class NewBoardFragment : Fragment() {
             }
         }
 
+    }
+
+    /**
+     * Called when a touch event is dispatched to a view. This allows listeners to
+     * get a chance to respond before the target view.
+     *
+     * @param view The view the touch event has been dispatched to.
+     * @param event The MotionEvent object containing full information about
+     * the event.
+     * @return True if the listener has consumed the event, false otherwise.
+     */
+    override fun onTouch(view: View?, event: MotionEvent?): Boolean {
+        if (view != null && event != null) {
+            when (view) {
+                binding.moreBombsButton -> onTouchMoreBombsButton(view, event)
+                binding.lessBombsButton -> onTouchLessBombsButton(view, event)
+            }
+            return true
+        }
+        return false
+    }
+
+    private fun onTouchMoreBombsButton(view: View, event: MotionEvent) {
+        if (!isMotionEventInsideView(view, event)
+            || event.action == MotionEvent.ACTION_UP
+            || event.action == MotionEvent.ACTION_CANCEL)
+            moreBombsButtonClickedJob?.cancel()
+
+        else if (event.action == MotionEvent.ACTION_DOWN) {
+            moreBombsButtonClickedJob = lifecycleScope.launch {
+                var delayMillis = 400L
+                while (true) {
+                    viewModel.onMoreBombsClicked()
+                    // This is a suspend function so 'isActive' is evaluated
+                    delay(delayMillis)
+                    if (delayMillis > 100) delayMillis -= 100
+                }
+            }
+        }
+    }
+
+    private fun onTouchLessBombsButton(view: View, event: MotionEvent) {
+        if (!isMotionEventInsideView(view, event)
+            || event.action == MotionEvent.ACTION_UP
+            || event.action == MotionEvent.ACTION_CANCEL)
+            lessBombsButtonClickedJob?.cancel()
+
+        else if (event.action == MotionEvent.ACTION_DOWN) {
+            lessBombsButtonClickedJob = lifecycleScope.launch {
+                var delayMillis = 400L
+                while (true) {
+                    viewModel.onLessBombsClicked()
+                    // This is a suspend function so 'isActive' is evaluated
+                    delay(delayMillis)
+                    if (delayMillis > 100) delayMillis -= 100
+                }
+            }
+        }
+    }
+
+    private fun isMotionEventInsideView(view: View, event: MotionEvent): Boolean {
+        val viewRect = Rect(view.left, view.top, view.right, view.bottom)
+        return viewRect.contains(view.left + event.x.toInt(), view.top + event.y.toInt())
     }
 }
