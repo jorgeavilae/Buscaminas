634d8541ae267d7ada0cbcdc82da4026b106428d
jorgeavilae <jorgeavilae@gmail.com>
2020-05-22 19:44:58 +0200

    Added some content to "About" Activity.

--- app/src/main/java/com/jorgeav/buscaminas/ui/about/AboutActivity.kt
+++ app/src/main/java/com/jorgeav/buscaminas/ui/about/AboutActivity.kt
@@ -16,7 +16,11 @@
 
 package com.jorgeav.buscaminas.ui.about
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.jorgeav.buscaminas.R
 
@@ -27,5 +31,28 @@ class AboutActivity : AppCompatActivity() {
         setContentView(R.layout.activity_about)
 
         actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        findViewById<TextView>(R.id.about_dagger_link).apply {
+            setOnClickListener {
+                openLink(Uri.parse(text.toString()))
+            }
+        }
+
+        findViewById<TextView>(R.id.about_room_explorer_link).apply {
+            setOnClickListener {
+                openLink(Uri.parse(text.toString()))
+            }
+        }
+
+        findViewById<TextView>(R.id.about_icons_link).apply {
+            setOnClickListener {
+                openLink(Uri.parse(text.toString()))
+            }
+        }
+    }
+
+    private fun openLink(uri: Uri) {
+        Toast.makeText(this, R.string.opening_link, Toast.LENGTH_SHORT).show()
+        startActivity(Intent(Intent.ACTION_VIEW, uri))
     }
 }
