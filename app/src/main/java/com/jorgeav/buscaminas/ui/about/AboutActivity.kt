89b690ca4fd56b4fba498b2769dec6bfb3e12c79
jorgeavilae <jorgeavilae@gmail.com>
2020-05-23 16:49:30 +0200

    Added content and links to "About" Activity.

--- app/src/main/java/com/jorgeav/buscaminas/ui/about/AboutActivity.kt
+++ app/src/main/java/com/jorgeav/buscaminas/ui/about/AboutActivity.kt
@@ -16,11 +16,9 @@
 
 package com.jorgeav.buscaminas.ui.about
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.jorgeav.buscaminas.R
 
@@ -32,27 +30,12 @@ class AboutActivity : AppCompatActivity() {
 
         actionBar?.setDisplayHomeAsUpEnabled(true)
 
-        findViewById<TextView>(R.id.about_dagger_link).apply {
-            setOnClickListener {
-                openLink(Uri.parse(text.toString()))
-            }
-        }
-
-        findViewById<TextView>(R.id.about_room_explorer_link).apply {
-            setOnClickListener {
-                openLink(Uri.parse(text.toString()))
-            }
-        }
-
-        findViewById<TextView>(R.id.about_icons_link).apply {
-            setOnClickListener {
-                openLink(Uri.parse(text.toString()))
-            }
-        }
-    }
-
-    private fun openLink(uri: Uri) {
-        Toast.makeText(this, R.string.opening_link, Toast.LENGTH_SHORT).show()
-        startActivity(Intent(Intent.ACTION_VIEW, uri))
+        findViewById<TextView>(R.id.about_developer_text).movementMethod = LinkMovementMethod.getInstance()
+        findViewById<TextView>(R.id.about_github_text).movementMethod = LinkMovementMethod.getInstance()
+        findViewById<TextView>(R.id.about_dagger).movementMethod = LinkMovementMethod.getInstance()
+        findViewById<TextView>(R.id.about_room_explorer).movementMethod = LinkMovementMethod.getInstance()
+        findViewById<TextView>(R.id.about_icons).movementMethod = LinkMovementMethod.getInstance()
+        findViewById<TextView>(R.id.about_eula_link).movementMethod = LinkMovementMethod.getInstance()
+        findViewById<TextView>(R.id.about_license_link).movementMethod = LinkMovementMethod.getInstance()
     }
 }
